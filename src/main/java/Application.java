5b74c2cdffbaa6c17af3670c91194018cfbda5da
Tiago Pinto <up202206280@up.pt>
2023-10-03 20:32:18 +0100

    step 7- add a moving 'x' char

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        Game game= new Game();
+        Game game = new Game();
         game.run();
+    }
 }
 
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,21 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+
 import java.io.IOException;
-import java.security.PublicKey;
+
+
 
 public class Game {
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
     public Game(){
         try {
 
@@ -17,10 +24,10 @@ public class Game {
                     DefaultTerminalFactory()
                     .setInitialTerminalSize(terminalSize);
             Terminal terminal = terminalFactory.createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
+            this.screen = new TerminalScreen(terminal);
+            this.screen.setCursorPosition(null); // we don't need a cursor
+            this.screen.startScreen(); // screens must be started
+            this.screen.doResizeIfNecessary(); // resize screen if necessary
 
         }
 
@@ -30,12 +37,45 @@ public class Game {
     }
 
     private void draw() throws IOException {
-        screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                [0]);
-        screen.refresh();
+        this.screen.clear();
+        this.screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        this.screen.refresh();
+    }
+   private void processKey(KeyStroke key) throws IOException {
+        System.out.println(key);
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                y -= 1;
+                break;
+            case ArrowLeft:
+                x -= 1;
+                break;
+            case ArrowDown:
+                y += 1;
+                break;
+
+            case ArrowRight:
+                x += 1;
+                break;
+            case Character:
+                if (key.getCharacter() == 'q' || key.getCharacter() == 'Q') {
+                    try {
+                        this.screen.close(); 
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
     }
     public void run() throws IOException{
+        while(true) {
         draw();
+            KeyStroke key = this.screen.readInput();
+            processKey(key);
+            if (key.getKeyType() == KeyType.EOF) {
+                break;
+            }
+        }
     }
 }
