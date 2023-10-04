6255342d076018a0a9228278d29047e2513b6957
Tiago Pinto <up202206280@up.pt>
2023-10-04 16:35:43 +0100

    Step 8 - added a new class called 'Hero'

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private Hero hero;
     private int x = 10;
     private int y = 10;
     public Game(){
@@ -24,11 +25,12 @@ public class Game {
                     DefaultTerminalFactory()
                     .setInitialTerminalSize(terminalSize);
             Terminal terminal = terminalFactory.createTerminal();
+
             this.screen = new TerminalScreen(terminal);
             this.screen.setCursorPosition(null); // we don't need a cursor
             this.screen.startScreen(); // screens must be started
             this.screen.doResizeIfNecessary(); // resize screen if necessary
-
+            this.hero = new Hero(10, 10);
         }
 
         catch (IOException e) {
@@ -38,29 +40,29 @@ public class Game {
 
     private void draw() throws IOException {
         this.screen.clear();
-        this.screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        this.hero.draw(screen);
         this.screen.refresh();
     }
    private void processKey(KeyStroke key) throws IOException {
         System.out.println(key);
         switch (key.getKeyType()) {
             case ArrowUp:
-                y -= 1;
+                hero.moveUp();
                 break;
             case ArrowLeft:
-                x -= 1;
+                hero.moveLeft();
                 break;
             case ArrowDown:
-                y += 1;
+                hero.moveDown();
                 break;
 
             case ArrowRight:
-                x += 1;
+                hero.moveRight();
                 break;
             case Character:
                 if (key.getCharacter() == 'q' || key.getCharacter() == 'Q') {
                     try {
-                        this.screen.close(); 
+                        this.screen.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
